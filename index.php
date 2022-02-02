6a9b412c714a580e419ea97f4a28b3434b108cd9
leandro falcao <leandrofalcaoti@gmail.com>
2022-02-02 19:04:30 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+session_start(); //Iniciar a sessao
+
+include_once "conexao.php";
+?>
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="./css/main.css">
+    <script src="./js/main.js"></script>
+
+    <title>listando dados</title>
+</head>
+
+<body>
+
+    <div id="content-index">
+        <header id="topo">
+            <div id="logo-card">
+                <img src="https://www.brbcard.com.br/Portal/Frontend/img/logo_negativa.png.pagespeed.ce.HmKzAhzPcS.png" alt="brbcard">
+            </div>
+            <div></div>
+            <div></div>
+        </header>
+        <h1>Listar Usuários</h1>
+        <div id="links-uteis-index">
+            <a href="listar.php" style="padding-right: 0.42rem;">Listar</a><br>
+            <a href="cadastrar.php">Cadastrar</a>
+        </div>
+    </div>
+
+    <?php
+
+    ?>
+
+</body>
+
+</html>
